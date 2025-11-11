e9a38bd71706f8c86754bf9f82bf2d3347fd755b
Juan.David.Mosquera.Palacios <juandavidmospa12@gmail.com>
2025-11-10 20:27:07 -0500

    actualizacion

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,6 @@ export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
-    colors: {
-      'primary': '#8c0096ff',
-      'secondary': '#FFC107',
-      'accent': '#FF5722',
-      'dark': '#212121',
-      'light': '#FFFFFF',
-      'error': '#F44336',
-      'success': '#4CAF50',
-      'warning': '#FF9800',
-    }
-  },
   plugins: [],
 }
+};
