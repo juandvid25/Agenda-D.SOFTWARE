b7393e06fc4c70fec4e6495af13503e73d5d748c
Juan.David.Mosquera.Palacios <juandavidmospa12@gmail.com>
2025-11-24 19:12:58 -0500

    Clase_9_Agenda_ADSO_v7_Refactor_Documentacion

--- src/App.jsx
+++ src/App.jsx
@@ -116,6 +116,8 @@ import {
   eliminarContactoPorId,
 } from "./api";
 
+import { APP_INFO } from "./config";
+
 import FormularioContacto from "./components/FormularioContacto";
 import ContactoCard from "./components/ContactoCard";
 
@@ -188,13 +190,13 @@ function App() {
 
         <header className="mb-8">
           <p className="text-xs tracking-[0.3em] text-gray-500 uppercase">
-            Desarrollo Web ReactJS Ficha 3223876
+            Desarrollo Web ReactJS Ficha {APP_INFO.ficha}
           </p>
           <h1 className="text-4xl font-extrabold text-gray-900 mt-2">
-            Agenda ADSO v6
+           {APP_INFO.titulo}
           </h1>
           <p className="text-sm text-gray-600 mt-1">
-            Gestión de contactos conectada a una API local con JSON Server.
+          {APP_INFO.subtitulo}
           </p>
         </header>
 
--- src/api.jsx
+++ src/api.jsx
@@ -1,15 +1,16 @@
-const API = "http://localhost:3002/contactos";
+// Importamos la URL base desde config.js
+import { API_BASE_URL } from "./config";
 
 // GET: listar contactos
 export async function listarContactos() {
-  const res = await fetch(API);
+  const res = await fetch(API_BASE_URL );
   if (!res.ok) throw new Error("Error al listar contactos");
   return res.json();
 }
 
 // POST: crear contacto
 export async function crearContacto(data) {
-  const res = await fetch(API, {
+  const res = await fetch(API_BASE_URL , {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
@@ -20,7 +21,7 @@ export async function crearContacto(data) {
 
 // DELETE: eliminar contacto por id
 export async function eliminarContactoPorId(id) {
-  const res = await fetch(`${API}/${id}`, {
+  const res = await fetch(`${API_BASE_URL }/${id}`, {
     method: "DELETE",
   });
   if (!res.ok) throw new Error("Error al eliminar el contacto");
